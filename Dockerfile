f9a78493594e4cd8b17e9577f65289849feb66a0
Eric1015 <love1dbsb77@hotmail.com>
2019-11-23 18:08:30 -0800

    Implemented add user function as one of the endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM postgres
-ENV POSTGRES_DB postgres
+ENV POSTGRES_DB census
 COPY psql_dump.sql /docker-entrypoint-initdb.d/
--- server/handler.js
+++ server/handler.js
@@ -1,6 +1,20 @@
 'use strict';
 const pg = require('pg');
-const conString = "postgres://posgres@localhost:5432/postgres";
+const conString = "postgres://postgres@localhost:5432/census";
+const Sequelize = require('sequelize');
+const config = require('./config/config.json');
+const { database, username, password, host } = config.development;
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  dialect: 'postgres',
+  define: {
+    timestamps: false,
+  }
+});
+const UserModel = require('./models/user');
+
+// models
+const User = UserModel(sequelize, Sequelize);
 
 module.exports.hello = async event => {
   var client = new pg.Client(conString);
@@ -19,4 +33,34 @@ module.exports.hello = async event => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.createUser = async event => {
+  let obj = JSON.parse(event.body);
+  const user = User.build(obj);
+  return user.save().then(() => {
+    return {
+      statusCode: 201,
+      body: JSON.stringify(
+        {
+          message: 'Sign up successfully',
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  }).catch((err) => {
+    return {
+      statusCode: 422,
+      body: JSON.stringify(
+        {
+          message: err,
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  })
 };
